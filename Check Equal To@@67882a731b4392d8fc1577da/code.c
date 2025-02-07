3afb4bfb195e51761a4f8715d13832d7996fb27b
Prachisindhi <prachisindhi47@gmail.com>
2025-02-07 14:24:13 +0530

    Solution to Check Equal To

--- Check Equal To@@67882a731b4392d8fc1577da/code.c
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -2,11 +2,11 @@
 
 int main(){
     int a,b;
-    printf("Enter two space separated integers:");
+    
     scanf("%d %d",&a,&b);
     if(a==b)
     printf("True\n");
     else
     printf("False\n");
-    reverse 0;
+    return 0;
 }
